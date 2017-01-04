4efbcef4969378f5d6a5b63812c76f697160d15f
pedula <renato.pedula@sr2.uerj.br>
2017-01-04 00:31:50 -0200

    alteracao na migration

--- suites/migrations/0001_initial.py
+++ suites/migrations/0001_initial.py
@@ -14,8 +14,12 @@ class Migration(migrations.Migration):
             name='ControleQuarto',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('data_inicio', models.DateTimeField(null=True, blank=True)),
-                ('data_fim', models.DateTimeField(null=True, blank=True)),
+                ('data_inicio', models.DateTimeField()),
+                ('data_fim', models.DateTimeField()),
+                ('diaria', models.IntegerField(null=True, blank=True)),
+                ('qtd_dias', models.IntegerField(null=True, blank=True)),
+                ('valor_reserva', models.FloatField(null=True, blank=True)),
+                ('valor_total', models.FloatField(null=True, blank=True)),
             ],
             options={
                 'db_table': 'ControleQuarto',
@@ -32,13 +36,15 @@ class Migration(migrations.Migration):
             ],
             options={
                 'db_table': 'NomeQuarto',
+                'verbose_name': 'nome do quarto',
+                'verbose_name_plural': 'nome dos quartos',
             },
             bases=(models.Model,),
         ),
         migrations.AddField(
             model_name='controlequarto',
-            name='NomeQuartos',
-            field=models.ManyToManyField(to='suites.NomeQuarto', null=True, blank=True),
+            name='nomeQuartos',
+            field=models.ManyToManyField(to='suites.NomeQuarto'),
             preserve_default=True,
         ),
     ]
